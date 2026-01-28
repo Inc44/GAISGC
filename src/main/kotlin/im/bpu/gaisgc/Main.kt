b30914161cd492734564d55605a68eba989e842c
Inc44 <incognita44@outlook.com>
2026-01-28 22:13:52 +0100

    minimum size

--- src/main/kotlin/im/bpu/gaisgc/Main.kt
+++ src/main/kotlin/im/bpu/gaisgc/Main.kt
@@ -6,11 +6,13 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
 import im.bpu.gaisgc.ui.ApplicationLayout
+import java.awt.Dimension
 
 fun main() = application {
 	val APPLICATION_NAME = "GAISGC"
 	val state = WindowState(size = DpSize(1200.dp, 800.dp))
 	Window(onCloseRequest = ::exitApplication, state = state, title = APPLICATION_NAME) {
+		window.minimumSize = Dimension(960, 600)
 		ApplicationLayout()
 	}
 }
