3bc188e06d4291e319e255a9f533bda7ac56c0a5
Inc44 <incognita44@outlook.com>
2026-02-08 01:11:46 +0100

    bugfix

--- src/main/kotlin/im/bpu/gaisgc/model/Item.kt
+++ src/main/kotlin/im/bpu/gaisgc/model/Item.kt
@@ -3,10 +3,10 @@ package im.bpu.gaisgc.model
 data class Item(
 	val createdTime: Long = 0L,
 	val fileExtension: String? = null,
-	val id: String,
+	val id: String = "",
 	val mimeType: String = "",
 	val modifiedTime: Long = 0L,
-	val name: String,
+	val name: String = "",
 	val sha256Checksum: String? = null,
 	val size: Long = -1L,
 	val isNotFound: Boolean = false,
